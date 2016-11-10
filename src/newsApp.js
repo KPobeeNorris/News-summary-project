6bb06fdb7bbad2a23fb6e58d14486790620e6404
Anna Lucking <annavlucking@hotmail.co.uk>
2016-11-10 17:15:52 +0000

    add full story function - no callbacks

--- src/newsApp.js
+++ src/newsApp.js
@@ -15,14 +15,26 @@ var getJSON = function(url) {
   });
 };
 
-getJSON('https://content.guardianapis.com/search?api-key=f82b7327-9c62-42a9-a8e5-fb351c498b90').then(function(data) {
+getJSON('https://content.guardianapis.com/search?show-fields=body&api-key=f82b7327-9c62-42a9-a8e5-fb351c498b90').then(function(data) {
     // alert('Your Json result is:  ' + data);
+    console.log(2);
     var myDiv = document.getElementById('headline');
     var headline = "";
     for (i=0; i < data.response.results.length; i++) {
-      headline += data.response.results[i].webTitle + "<br>"
+      headline += data.response.results[i].webTitle + "<br>";
       myDiv.innerHTML = headline;
     }
 }, function(status) { //error detection....
   alert('Something went wrong.');
 });
+
+getJSON('http://content.guardianapis.com/search?show-fields=body&api-key=f82b7327-9c62-42a9-a8e5-fb351c498b90').then(function(data) {
+  // function displayBody(data) {
+    console.log(1);
+    var myDiv2 = document.getElementById('fullArticle');
+    var fullBody = data.response.results[1].fields.body;
+    console.log(fullBody);
+    myDiv2.innerHTML = fullBody;
+  }, function(status) { //error detection....
+    alert('Something went wrong.');
+  });
