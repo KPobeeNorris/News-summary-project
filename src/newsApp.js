e765a070e00da607d238346f4ec4001157ef60d5
Dionysis <Dionysis.k22@gmail.com>
2016-11-10 16:21:14 +0000

    for each headline i have a summary

--- src/newsApp.js
+++ src/newsApp.js
@@ -15,14 +15,25 @@ var getJSON = function(url) {
   });
 };
 
-getJSON('https://content.guardianapis.com/search?api-key=f82b7327-9c62-42a9-a8e5-fb351c498b90').then(function(data) {
+getJSON('http://content.guardianapis.com/search?show-fields=body&api-key=f82b7327-9c62-42a9-a8e5-fb351c498b90').then(function(data) {
     // alert('Your Json result is:  ' + data);
     var myDiv = document.getElementById('headline');
     var headline = "";
     for (i=0; i < data.response.results.length; i++) {
-      headline += data.response.results[i].webTitle + "<br>"
+      headline += data.response.results[i].webTitle + "<br>"+ "<br>"
       myDiv.innerHTML = headline;
     }
-}, function(status) { //error detection....
-  alert('Something went wrong.');
-});
+    var mySumDiv = document.getElementById('summary');
+    var summary = "";
+    for (i=0; i < data.response.results.length; i++) {
+      var sumUrl = data.response.results[i].webUrl
+      getJSON('http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url='+sumUrl).then(function(dataSum) {
+        summary += dataSum.sentences + "<br>"+ "<br>"
+        mySumDiv.innerHTML = summary;
+      })
+    };
+})
+
+
+
+// console.log(data.response.results[1].webTitle)
