ff634254345de3bc8938b76702932aa8f754aa84
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-11-10 18:12:31 +0000

    Click link

--- src/newsApp.js
+++ src/newsApp.js
@@ -1,3 +1,7 @@
+var NewsApp = function () {
+  this.summarisedArticle = "";
+};
+
 var getJSON = function(url) {
   return new Promise(function(resolve, reject) {
     var xhr = new XMLHttpRequest();
@@ -20,9 +24,28 @@ getJSON('https://content.guardianapis.com/search?api-key=f82b7327-9c62-42a9-a8e5
     var myDiv = document.getElementById('headline');
     var headline = "";
     for (i=0; i < data.response.results.length; i++) {
-      headline += data.response.results[i].webTitle + "<br>"
+      var article = data.response.results[i].webTitle;
+      var link = data.response.results[i].webUrl;
+      headline += "<a id=link" + i + " href=#" + article + "</a>" + article + "<br>";
       myDiv.innerHTML = headline;
     }
 }, function(status) { //error detection....
   alert('Something went wrong.');
 });
+
+NewsApp.prototype.summarise = function(link) {
+  var summarisedArticle = "http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=" + link;
+  return summarisedArticle; 
+};
+
+function clickCreateSummary() {
+  document
+  .getElementById(link)
+  .addEventListener("click", function(clickEvent){
+    clickEvent.preventDefault();
+    summarise(headline);
+  });
+}
+
+clickCreateSummary();
+summarise(link);
